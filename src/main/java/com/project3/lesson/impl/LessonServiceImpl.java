0c78aca91344b0fbcd319769a697a947c7939571
songminyoung <lavel623@naver.com>
2022-07-13 17:31:19 +0900

    LessonService 주석추가 LessonServiceImpl 기능 추가

--- src/main/java/com/project3/lesson/impl/LessonServiceImpl.java
+++ src/main/java/com/project3/lesson/impl/LessonServiceImpl.java
@@ -12,14 +12,36 @@ public class LessonServiceImpl implements LessonService{
 	@Autowired
 	private LessonDAO lessonDAO;
 	
+	@Override
 	public LessonVO getLesson(LessonVO vo) {
 		return lessonDAO.getLesson(vo);
 		// 강의 하나하나 불러오는거
 	}
 
+	@Override
 	public List<LessonVO> getLessonList(LessonVO vo) {
 		return lessonDAO.getLessonList(vo);
 		// 강의 리스트를 불러오는거
 	}
+
+	@Override
+	public boolean updateLesson(LessonVO vo) {
+		return false;
+		// 강의 수정 - 기본값 실패
+	}
+
+	@Override
+	public boolean deleteLesson(LessonVO vo) {
+		return false;
+		// 강의 삭제 - 기본값 실패
+	}
+	
+	@Override
+	public boolean insertLesson(LessonVO vo) {
+		return false;
+		// 강의 등록 - 기본값 실패
+	}
+
+	
 	
 }
