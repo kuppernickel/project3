5fbf106a800c86c64766fee4171ff2f9e7802d4a
kuppernickel <kuppernickel@gmail.com>
2022-07-14 18:02:35 +0900

    유저 테이블 칼럼 변경에 맞춰서 쿼리문 맵핑 및 vo 수정

--- src/main/java/com/project3/biz/user/UserVO.java
+++ src/main/java/com/project3/biz/user/UserVO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserVO {
-	private String userId;				// 로그인 아이디(학번, 직번)
+	private String userId;			// 로그인 아이디(학번, 직번)
 	private String auth;			// 권한
 	private String pw;				// 비밀번호
 	private String name;			// 이름
@@ -16,4 +16,6 @@ public class UserVO {
 	private String zipCode;			// 우편번호
 	private String address1;		// 주소
 	private String address2;		// 상세주소
+	private int level;				// 학년
+	private String enroll;			// 재적
 }
