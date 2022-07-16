1a4eb823d946e09db29b75c82a7ff4afab8043aa
poohharry <poohharry@naver.com>
2022-07-16 20:08:51 +0900

    과목 리스트 관련 코드 임시 저장 컨트롤러에 유저의 정보를 받아오는 내용 추가

--- src/main/java/com/project3/view/lesson/SubjectController.java
+++ src/main/java/com/project3/view/lesson/SubjectController.java
@@ -1,16 +1,19 @@
 package com.project3.view.lesson;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.project3.biz.subject.SubjectService;
 import com.project3.biz.subject.SubjectVO;
+import com.project3.biz.user.UserVO;
 
 @Controller
 @SessionAttributes("subject")
@@ -20,16 +23,21 @@ public class SubjectController {
 	private SubjectService subjectService;
 	
 	@RequestMapping(path = "/getSubject.do", method = RequestMethod.GET)
-	public String getSubject(HttpServletRequest request, SubjectVO vo, Model model) {
-		System.out.println("이동하는 과목 페이지 : " + request.getParameter("subject"));
-		model.addAttribute("subject", subjectService.getSubject(vo));
+	public String getSubject(@RequestParam String subjectCode, SubjectVO vo, Model model) {
+		System.out.println("이동하는 과목페이지의 과목코드 : " + subjectCode);
 		// 파라미터로 받아온 과목명에 따라 lectureList가 각기 다른 내용을 보여줘야함 -> 모델에 담아야 한다
+		vo.setSubjectCode(subjectCode);
+		model.addAttribute("subject", subjectService.getSubject(vo));
 		return "/jsp/lectureList.jsp";
 	}
 	
 	@RequestMapping(path = "/getSubjectList.do", method = RequestMethod.GET)
-	public String getSubjectList() {
+	public String getSubjectList(HttpSession session, SubjectVO vo, Model model) {
+		UserVO user = (UserVO) session.getAttribute("user");
+		
+		System.out.println();
 		// 유저의 정보를 받아서 그 유저가 수강한 과목들을 모델에 담아야한다.
+		model.addAttribute("subject", subjectService.getSubject(vo));
 		return "/jsp/detailedLectureList.jsp";
 	}
 }
