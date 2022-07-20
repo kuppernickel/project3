7f5c25bd154ac229eb0b7d6ef0824a370be64a19
kuppernickel <kuppernickel@gmail.com>
2022-07-20 12:46:25 +0900

    과목 리스트 출력 관련 주석

--- src/main/java/com/project3/view/lesson/SubjectController.java
+++ src/main/java/com/project3/view/lesson/SubjectController.java
@@ -36,9 +36,9 @@ public class SubjectController {
 			@RequestParam(value="day", required=false) String day,
 			UserVO vo, Model model) {
 		vo = (UserVO) session.getAttribute("user");
-		if(day!=null) vo.setDay(day);
+		if(day!=null) vo.setDay(day); //user가 담고있는 현재 요일(한국표준시 기준)을 디폴트로 설정
 		
-		System.out.println("day:"+vo.getDay());
+		System.out.println("day:"+vo.getDay()); //요일별 과목 검색
 		// 유저의 정보를 받아서 그 유저가 수강한 과목들을 모델에 담아야한다.
 		model.addAttribute("subjectList", subjectService.getSubjectList(vo));
 		return "/jsp/subjectList.jsp";
